0446353adfaff95df30fc5d7dc2f0648c6fe5429
igalv <vilenskyigal@gmail.com>
2021-07-26 14:56:33 +0200

    react components setup and redux setup

---
+++ src/components/Products/Product/Products.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./products.scss";
+function Products() {
+  return (
+    <div className="products">
+      <h2>Product component</h2>
+    </div>
+  );
+}
+
+export default Products;
---
+++ src/components/SingleItem/SingleItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./singleItem.scss";
+export default function SingleItem() {
+  return (
+    <div className="singleItem">
+      <h1>Single Item (Product component)</h1>
+    </div>
+  );
+}
